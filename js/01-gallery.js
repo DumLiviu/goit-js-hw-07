bbd9f46e74983256ec639bfb63c22fbb793d185c
DumLiviu <158556659+DumLiviu@users.noreply.github.com>
2024-06-22 09:45:09 +0300

    Finalizare tema
    Indeplinire sarcini tema

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const ulElements = document.querySelector(".gallery");
+
+galleryItems.forEach((element) => {
+  const liElements = document.createElement("li");
+  liElements.classList.add("gallery__item");
+  liElements.innerHTML = `<a class='gallery__link' 
+  href='${element.original}'>
+  <img class="gallery__image"
+  src='${element.preview}'
+  data-source='${element.original}'
+  alt='${element.description}' />
+  </a>
+  `;
+  ulElements.append(liElements);
+});
+ulElements.addEventListener("click", imageView);
+
+function imageView(e) {
+  const clicked = e.target;
+  e.preventDefault();
+  if (clicked.nodeName !== "IMG") {
+    return;
+  }
+  basicLightbox
+    .create(`<img width="1400" height="900" src="${clicked.dataset.source} ">`)
+    .show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const ulElement =
+  document.querySelector(".gallery");
+
+galleryItems.forEach((item) => {
+  const liItems = document.createElement("li");
+  liItems.classList.add("gallery__item");
+  liItems.innerHTML = `<a class="gallery__link" href='${item.original}'>
+    <img class="gallery__image" src='${item.preview}' alt='${item.description}' />
+    </a>`;
+  ulElement.append(liItems);
+});
+
+const lightbox = new SimpleLightbox(
+  ".gallery a",
+  {
+    captions: true,
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+  }
+);
